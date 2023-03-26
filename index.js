f97504c6e99be795bcd8d69ad7189676ca77566e
DenysFront <i(~B4tkDE6U5+d9>
2023-03-26 15:53:47 +0300

    Post method has been added

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 import express from 'express'
-console.log('Server started!')
+import mongoose from 'mongoose'
+import Post from './Post.js'
+
+const URL_DB = `mongodb+srv://aptikieievdenys:FLRKoaNIhOX3HC2F@cluster0.wla6lh3.mongodb.net/?retryWrites=true&w=majority`
+
+const PORT = 5000;
+
+const app = express();
+
+app.post('/', async (req, res) => {
+  const {author, title, picture, content} = req.body
+  const post = await Post.create({author, title, picture, content})
+  res.status(200).json('Server are working')
+})
+
+async function startApp() {
+  try {
+   await mongoose.connect(URL_DB)
+    app.listen(PORT, () => console.log("Server Started!" + PORT))
+  } catch(e) {
+    console.log(e)
+  }
+}
+
+startApp()
+
